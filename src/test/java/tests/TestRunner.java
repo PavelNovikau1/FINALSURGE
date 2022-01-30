28a826458b52406c07ecf5d9fa8c116bb21fed28
Pavel <razor8ne8@icloud.com>
2022-01-30 14:08:05 +0200

    add parallel launch

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -18,4 +18,10 @@ import utils.TestListener;
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
